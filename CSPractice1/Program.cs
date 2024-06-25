432e1c6c5f8ece89312fdf76220c0512f9c52ee4
kim <ksh251024@gmail.com>
2024-06-25 10:09:21 +0900

    #1 12-2 출력

--- CSPractice1/Program.cs
+++ CSPractice1/Program.cs
@@ -13,14 +13,16 @@ namespace CSPractice1
         {
             // 컨텍스트 키워드
             var abc = "Hello World"; // OK
-          /***  XML 주석  ***/
-          /*
-           *여러줄 주석 
-           * 
-           */
-          //한 줄 주석
-          
+            /***  XML 주석  ***/
+            /*
+             *여러줄 주석 
+             * 
+             */
+            //한 줄 주석
 
+            // 출력
+            Console.WriteLine("hello ");
+            Console.Write("wolrd 개행x");
 
         }
     }
