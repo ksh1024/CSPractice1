d1296d1c4d694616bf0431f223b696b4479bcde0
kim <ksh251024@gmail.com>
2024-07-01 13:32:30 +0900

    #4 21-4. 인스턴스 변수

---
+++ CSPractice1/User.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSPractice1
+{
+    internal class User
+    {
+        public string name;
+        public string phoneNumber;
+    }
+
+    class Product
+    {
+        public static double TAX_RATIO = 1.0; // 클래스 변수(정적 변수)
+        public string name; // 인스턴스 변수
+        public int price; // 인스턴스 변수
+
+    }
+}
